bffee05423f875f43e0eb4bc6304d2524817065b
Fabrizio Mesto <fabrizio.mesto@telntsrl.com>
2017-11-10 13:07:53 +0100

    begin

---
+++ fab-import.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Fab Import
+Plugin URI: https://www.netedit.it/
+Description: Plugin Fab Import
+Author: Fabrizio MESTO
+Version: 0.0.1
+Author URI: https://www.netedit.it/
+Text Domain: fabtest
+Domain Path: lang
+*/
+
+
+class Fab_Import {
+  public function __construct() {
+    
+  }
+}
+
+
+$fab_import = new Fab_Import();
